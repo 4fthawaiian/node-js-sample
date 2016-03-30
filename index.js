09ecc6ee8af146d40efaad1b6ffc96fc459995b0
Brad McCrorey <bradmccrorey@giantmacbookpro.local>
2016-03-30 12:11:09 -0500

    updated to use TestRun argument

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var myArgs = process.argv.slice(2);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -8,6 +9,16 @@ app.get('/', function(request, response) {
   response.send('Hello Foo!')
 })
 
-app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+if(myArgs[0] === "TestRun") {
+    console.log("Test run - will exit after 10 seconds");
+    setTimeout(function () {
+        server.close(function () {
+            console.log("exiting gracefully");
+            process.exit(0);
+        });
+    }, 10000)
+}
